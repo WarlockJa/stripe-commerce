28764c1222246f0c80608eba98f924c1d53932a2
warlockja <warlockja@gmail.com>
2025-08-14 11:05:20 +0300

    testing cloudflare deploy issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import ShopPage from "@/components/shop/shop-page";
 import StripePage from "@/components/stripe-page";
 
 export const runtime = "edge";
+// test
 
 export default function Home() {
   return (
